e53a889ea2d9c219249fc4e9352716d30e825b3b
JuFortini <jucimaraafortini@gmail.com>
2022-01-17 14:57:55 -0300

    componentizing

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,29 @@
-export function Content() {
+import { MovieCard } from "./MovieCard";
+
+interface MovieShown {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface MoviesData {
+  movieData: MovieShown[];
+}
+
+export function Content({ movieData }: MoviesData) {
   // Complete aqui
+  return (
+    <main>
+      <div className="movies-list">
+        {movieData.map(movie => (
+          <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+        ))}
+      </div>
+    </main>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,36 @@
-export function SideBar() {
+import { Button } from "./Button";
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  onGenre: GenreResponseProps[];
+  onGenreButton: (id: number) => void;
+  onSelectedGenreId: number;
+}
+
+export function SideBar({onGenre, onGenreButton, onSelectedGenreId}: SideBarProps) {
   // Complete aqui
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {onGenre.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onGenreButton(genre.id)}
+            selected={onSelectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
